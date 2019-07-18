b3eba1d76ac4105ada93f01aa1af711ee549d7ca
Дмитрий Бубненков <bubnenkov@fas.gov.ru>
2019-07-18 16:48:39 +0300

    second task

---
+++ xml2csv.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+import zipfile
+import xml.etree.ElementTree as ET
+
+arch_folder = "archives"
+csv_1_dict = dict() # тут по идее строка будет уникальнее чем число
+csv_2_dict = dict() # ключем будет значение из object
+
+# в данном мы геренирируем всего 2 csv файла для всех xml
+# т.к. XML у секций значения случайны, но не уникальны, то в данной реализации могут быть коллизии т.к. для формирования мы используем словари
+# пример второго csv файла:
+## maLryz jxIteh
+## jtzKlc Bgklrt
+## ZvFPFB nlERio
+## CevoJO nlERio
+## wcjZtl nlERio
+## pGwEou dkfWjd
+## ttGVYb dkfWjd
+## yCvCUs dkfWjd
+
+# никаких оптимизаций по производительности в данной версии нет
+
+def parse_xml(file):
+    csv_1_list = []
+    root = ET.parse(file)
+    id_value = '' # запомним его для указанного файла
+    name_value_list = [] # список всех значений имен в objects
+    
+    for elem in root.findall('.//var'):
+        level_value = ''
+        if elem.attrib.get('name') == 'id':
+            id_value = elem.attrib.get('value')
+
+        if elem.attrib.get('name') == 'level':
+            level_value = elem.attrib.get('value')
+
+        csv_1_dict[id_value] = level_value
+
+
+    for elem in root.findall('.//object'):
+        name_value = elem.attrib.get('name')
+        name_value_list.append(name_value)
+
+    for el in name_value_list:
+        csv_2_dict[el] = id_value
+
+
+for arch in Path(arch_folder).glob("*.zip"):
+    zip_file = zipfile.ZipFile(arch)
+    zip_file.extractall(Path(arch_folder, arch.stem))
+
+    for file in Path(arch_folder, arch.stem).glob("*.xml"):
+        print(file)
+        parse_xml(file)
+    print(arch)
+
+
+with open('csv_1.csv', 'w') as f:
+    for key, value in csv_1_dict.items():
+        # print(key," ", value)
+        # print(key, value)
+        f.write(key + " " + value + "\n") 
+
+
+with open('csv_2.csv', 'w') as f:
+    for key, value in csv_2_dict.items():
+        f.write(key + " " + value + "\n") 
